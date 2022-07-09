83467ac8174a8f28cf2ed347fbe06cda5b8cd19c
Logan Harber <chalharb@gmail.com>
2022-07-08 20:34:34 -0500

    Created boilerplate code

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+
+console.log('Hello World');
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
